cf49f6b950d775e84c5d514c9cb26f798d07bb30
Gabrielle Ednie <gabrielle.ednie@apexrms.com>
2022-11-24 13:48:52 -0500

    adding src folder

---
+++ src/model.R
@@ -0,0 +1,68 @@
+library(rsyncrosim)       # Load SyncroSim R package
+library(raster)          # Load raster package
+myScenario <- scenario()  # Get the SyncroSim Scenario that is currently running
+
+# Retrieve the transfer directory for storing output rasters
+e <- ssimEnvironment()
+transferDir <- e$TransferDirectory
+
+# Load RunControl datasheet to be able to set Timesteps
+runSettings <- datasheet(myScenario, name = "helloworldConda_RunControl")
+
+# Set Timesteps - can set to different frequencies if desired
+timesteps <- seq(runSettings$MinimumTimestep, runSettings$MaximumTimestep)
+
+# Load scenario's input datasheet from SyncroSim library into R dataframe
+myInputDataframe <- datasheet(myScenario,
+                              name = "helloworldConda_InputDatasheet")
+
+# Extract model inputs from complete input dataframe
+mMean <- myInputDataframe$mMean
+mSD <- myInputDataframe$mSD
+
+# Load raster input 
+rasterMap <- datasheetRaster(myScenario,
+                             datasheet = "helloworldConda_InputDatasheet",
+                             column = "InterceptRasterFile")
+
+# Setup empty R dataframe ready to accept output in SyncroSim datasheet format
+myOutputDataframe <- datasheet(
+  myScenario,
+  name = "helloworldConda_IntermediateDatasheet"
+)
+
+# For loop through iterations
+for (iter in runSettings$MinimumIteration:runSettings$MaximumIteration) {
+  
+  # Extract a slope value from normal distribution
+  m <- rnorm(n = 1, mean = mMean, sd = mSD)
+  
+  # Use each cell in the raster as the intercept in linear equation
+  newRasterMaps <- calc(rasterMap, function(b) m * timesteps + b,
+                        forceapply = TRUE)
+  
+  # The y value will be the sum of all the cells in each raster
+  y <- cellStats(newRasterMaps, stat = 'sum')
+  
+  # Add the new raster for this timestep/iteration to the output
+  newRasterNames <- file.path(paste0(transferDir, 
+                                     "/rasterMap_iter", iter, "_ts",
+                                     timesteps, ".tif"))
+  writeRaster(newRasterMaps, filename = newRasterNames,
+              format = "GTiff", overwrite = TRUE, bylayer = TRUE)
+  
+  # Store the relevant outputs in a temporary dataframe
+  tempDataframe <- data.frame(Iteration = iter,
+                              Timestep = timesteps,
+                              y = y,
+                              OutputRasterFile = newRasterNames)
+  
+  # Copy output into this R dataframe
+  myOutputDataframe <- addRow(myOutputDataframe, tempDataframe)
+}
+
+# Save this R dataframe back to the SyncroSim library's output datasheet
+saveDatasheet(myScenario,
+              data = myOutputDataframe,
+              name = "helloworldConda_IntermediateDatasheet")
+
